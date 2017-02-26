e00bd0766aa293219c25fe11789bb8aeddb3c53e
LI SHUANG <809889839@qq.com>
2017-02-26 16:12:55 +0800

    继续学习怎么前后台传参
    继续学习怎么前后台传参

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
-//主要就是ajax部分，这里用到了JQuery中的$.ajax函数，详细用法请参照JQ文档
+charset='UTF-8';
 $.ajax({
-    'url':'login.php',//第一个参数url，PHP脚本的位置，我要把参数传到什么位置
-    'data':{"username":$('#userName').val(),"password":$('#password').val(),},//传递什么数据，这里我用的是json格式，如果不知道什么是json数据，可以自己搜索一下
+    'url':'../login.php',
+    'data':{"username":$('#userName').val(),"password":$('#password').val(),},
     'success':function(data){//success表示，当服务器返回数据成功后，该做什么，返回的数据储存在data中，我们直接把data传入函数中。
         switch(data.type){
             case 0:alert('账户不存在');break;
@@ -17,12 +17,12 @@ $.ajax({
             case 2:alert('密码错误');break;
         }
     },
-    'type':'post',//type是ajax的方法，可以用post可以用get，两种方法的区别可以自己查阅资料
-    'dataType':'json',//传递的数据类型，对应我上面的数据格式，这里用json。数据类型也可以是html/xml等
+    'type':'post',//ajax的方法，可以用post可以用get
+    'dataType':'json',//传递的数据类型，这里用json。也可以是html/xml等
 });
 
 //success函数，表示前端收到数据成功后，要做什么
-'success':function(data){//这里的data储存有后端php返回的json数据
+success=function(data){//这里的data储存有后端php返回的json数据
         switch(data.type){//以data.type为switch-case关键字，分类处理
             case 0:alert('账户不存在');break;//如果type=0，说明帐号密码都不匹配
             case 1:{//type=1，说明匹配成功，登录成功
